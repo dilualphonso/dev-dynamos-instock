7b215306d206badc01398c5284b758dde24a2e69
ilaria-longo <157503715+ilaria-longo@users.noreply.github.com>
2024-03-13 14:28:58 -0400

    modify font style folder.Converted fonts to .woff and .woff2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,21 @@ import { Link } from "react-router-dom";
 import "./Header.scss";
 
 const Header = () => {
-  <header className="header">
-    <Link to="/">
-      <div className="header__logo">
-        <img src={Logo} alt="Warehouse Logo" />
-      </div>
-    </Link>
-  </header>;
+  return (
+    <header className="header">
+      <Link to="/">
+        <div>
+          <div className="header__logo">
+            <img src={Logo} alt="Warehouse Logo" />
+          </div>
+          <div>
+            <button className="button-header">Warehouses</button>
+            <button className="button-header">Inventory</button>
+          </div>
+        </div>
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
