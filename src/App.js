8e00db44c16eba9e5440ab6863fdd430b454f5fa
ilaria-longo <157503715+ilaria-longo@users.noreply.github.com>
2024-03-12 22:51:56 -0400

    adding a header.jsx in a components folder. Set up the routes but they will need adjustements with naming conventions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Warehouses />} />
+        <Route path="/warehouses" element={<WarehousePage />} />
+        {/* <Route path ="/warehouses/:id"element={< />} /> */}
+        <Route path="/warehouses/add" element={<WarehouseAddPage />} />
+        <Route path="/warehouses/:id/edit" element={<WarehouseEditPage />} />
+        <Route path="/inventory" element={<InventoryPage />} />
+        {/* // <Route path ="/inventory/:id"element={< />} /> */}
+        <Route path="/inventory/add" element={<InventoryAddPage />} />
+        <Route path="/inventory/:id/edit" element={<InventoryEditPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+{
+  /* <div className="App">
+<header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit <code>src/App.js</code> and save to reload.
+  </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header>
+</div> */
+}
