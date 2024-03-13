173421e5859a0d5d3d8193a586def30aff8ec93a
klkode <katrinalkite@gmail.com>
2024-03-13 16:07:14 -0400

    Call install to make sure all dependcies are installed. Create scss styling file for WarehouseAddPage and import it to component. Add the heading and Link to the top of the page.

--- src/pages/WarehousesAddPage/WarehousesAddPage.jsx
+++ src/pages/WarehousesAddPage/WarehousesAddPage.jsx
@@ -1,4 +1,19 @@
+import "./WarehousesAddPage.scss";
+import { Link } from "react-router-dom";
+
 function WarehousesAddPage() {
-  return <div></div>;
+    
+
+    return (
+        <section>
+            <article>
+                <Link to="/warehouses"></Link>
+                <h2>Add Warehouse</h2>
+            </article>
+            {/* Warehouse Details Component */}
+            {/* Contact Details Component */}
+        </section>
+    );
 }
+
 export default WarehousesAddPage;
