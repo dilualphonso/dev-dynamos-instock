d2753fe5a4df45a99c2855f7b0edc755ef595963
eshicai <eshicai@gmail.com>
2024-03-13 23:52:47 -0700

    add items to inventory list

--- src/Components/InventoriesList/InventoriesList.jsx
+++ src/Components/InventoriesList/InventoriesList.jsx
@@ -3,22 +3,37 @@ import { InventoryItem } from '../InventoryItem/InventoryItem'
 
 export const InventoriesList = ({ inventories, warehouseId }) => {
   return (
-    <div>
-      <h2>Inventory</h2>
-      <ul>
-        {inventories
-          // .filter(
-          //   (inventory =>
-          //     inventory.item_name !== "Winter Jacket"
-          //     && inventory.item_name !== "Watch"
-          //     && inventory.item_name !== "Soap"))
-          .map(
-            (inventory =>
-              <InventoryItem
-                key={inventory.id}
-                inventory={inventory}
-                warehouseId={warehouseId} />))}
+    <section className="inventories-list">
+      <div className="inventories-list__header">
+        <h1 className="inventories-list__title">Inventory</h1>
+        <div className="inventories-list__actions">
+          <form action="/search" method="get" className="inventories-list__search-container">
+            <input type="search" className="inventories-list__search" placeholder="Search..."></input>
+          </form>
+          <button className="inventories-list__add-button">Add New Item</button>
+        </div>
+      </div>
+
+      <div className="inventories-list__labels">
+        <p className="inventories-list__label">INVENTORY ITEM</p>
+        <p className="inventories-list__label">CATEGORY</p>
+        <p className="inventories-list__label">STATUS</p>
+        <p className="inventories-list__label">QTY</p>
+        <p className="inventories-list__label">WAREHOUSE</p>
+        <p className="inventories-list__label">ACTIONS</p>
+      </div>
+
+      <ul className="inventories-list__items">
+        {inventories.map(
+          (inventory) => (
+            <InventoryItem
+              key={inventory.id}
+              inventory={inventory}
+              warehouseId={warehouseId} />
+          )
+        )}
       </ul>
-    </div>
+    </section>
   )
+
 }
