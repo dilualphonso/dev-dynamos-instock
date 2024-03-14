cddc0ef885c90ecc617f5ef4a65776d901689841
klkode <katrinalkite@gmail.com>
2024-03-14 01:27:58 -0400

    Add html elements to the component. Add styling classNames to each element.

--- src/components/WarehouseForm/WarehouseForm.jsx
+++ src/components/WarehouseForm/WarehouseForm.jsx
@@ -3,21 +3,61 @@ import "./WarehouseForm.scss";
 function WarehouseDetailsForm() {
     
 
+    // Leave error fields hidden until validation is introduced
     return (
-        <section className="w">
-            <form>
-                <fieldset>
+        <section className="warehouse-form">
+            <form className="warehouse-form__form">
+                <fieldset className="warehouse-form__details-container">
                     <legend>Warehouse Details</legend>
+
+                    <label className="warehouse-form__label" htmlFor="warehouseName">Warehouse Name</label>
+                    <input className="warehouse-form__input" type="text" name="warehouseName" id="warehouseName" placeholder="Warehouse Name"/>
+                    {false &&
+                    <label className="warehouse-form__label warehouse-form__label--error" htmlFor="contactName"></label>}
+
+                    <label className="warehouse-form__label" htmlFor="streetAddress">Street Address</label>
+                    <input className="warehouse-form__input" type="text" name="streetAddress" id="streetAddress" placeholder="Street Address"/>
+                    {false &&
+                    <label className="warehouse-form__label warehouse-form__label--error" htmlFor="streetAddress"></label>}
+
+                    <label className="warehouse-form__label" htmlFor="city">City</label>
+                    <input className="warehouse-form__input" type="text" name="city" id="city" placeholder="City"/>
+                    {false&&
+                    <label className="warehouse-form__label warehouse-form__label--error" htmlFor="city"></label>}
+
+                    <label className="warehouse-form__label" htmlFor="country">Country</label>
+                    <input className="warehouse-form__input" type="text" name="country" id="country" placeholder="Country"/>
+                    {false&&
+                    <label className="warehouse-form__label warehouse-form__label--error" htmlFor="country"></label>}
+
                 </fieldset>
-                <fieldset>
+                <fieldset className="warehouse-form__contact-container">
                     <legend>Contact Details</legend>
-                    <label></label>
-                    <input type="text"/>
-                    <label></label>
+
+                    <label className="warehouse-form__label" htmlFor="contactName">Contact Name</label>
+                    <input className="warehouse-form__input" type="text" name="contactName" id="contactName" placeholder="Contact Name"/>
+                    {false &&
+                    <label className="warehouse-form__label warehouse-form__label--error" htmlFor="contactName"></label>}
+
+                    <label className="warehouse-form__label" htmlFor="position">Position</label>
+                    <input className="warehouse-form__input" type="text" name="position" id="position" placeholder="Position"/>
+                    {false &&
+                    <label className="warehouse-form__label warehouse-form__label--error" htmlFor="position"></label>}
+
+                    <label className="warehouse-form__label" htmlFor="phoneNumber">Phone Number</label>
+                    <input className="warehouse-form__input" type="text" name="phoneNumber" id="phoneNumber" placeholder="Phone Number"/>
+                    {false&&
+                    <label className="warehouse-form__label warehouse-form__label--error" htmlFor="phoneNumber"></label>}
+
+                    <label className="warehouse-form__label" htmlFor="email">Email</label>
+                    <input className="warehouse-form__input" type="text" name="email" id="email" placeholder="Email"/>
+                    {false&&
+                    <label className="warehouse-form__label warehouse-form__label--error" htmlFor="email"></label>}
+
                 </fieldset>
-                <div>
-                    <button>Cancel</button>
-                    <button>Add Warehouse</button>
+                <div className="warehouse-form__button-container">
+                    <button className="warehouse-form__button warehouse-form__button--cancel">Cancel</button>
+                    <button className="warehouse-form__button warehouse-form__button--add">Add Warehouse</button>
                 </div>
             </form>
         </section>
