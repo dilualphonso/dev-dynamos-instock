7d59a093581b5ca43112b0ccc52bdc3d478bca89
ilaria-longo <157503715+ilaria-longo@users.noreply.github.com>
2024-03-16 19:13:34 -0400

    corrected backwords navigation

--- src/pages/InventoryEditPage/InventoryEditPage.jsx
+++ src/pages/InventoryEditPage/InventoryEditPage.jsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import "./InventoryEditPage.scss";
 
 function InventoryEditPage() {
+  // Get the inventory item id from the browser path
+  const { id } = useParams();
+
   return (
     <section className="edit-inventory">
       <article className="edit-inventory__heading-container">
-        <Link className="edit-inventory__back-link" to="/inventory"></Link>
+        <Link
+          className="edit-inventory__back-link"
+          to={`/inventory/${id}`}
+        ></Link>
         <h1 className="edit-inventory__heading">Edit Inventory</h1>
       </article>
     </section>
   );
 }
-
 export default InventoryEditPage;
