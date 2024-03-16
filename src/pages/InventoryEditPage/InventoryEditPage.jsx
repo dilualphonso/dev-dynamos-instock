d6e8c5a006182238acd4ebb8bbeb9a9d83a78519
ilaria-longo <157503715+ilaria-longo@users.noreply.github.com>
2024-03-16 18:44:15 -0400

    adding heading for edit inventory page

--- src/pages/InventoryEditPage/InventoryEditPage.jsx
+++ src/pages/InventoryEditPage/InventoryEditPage.jsx
@@ -1,4 +1,15 @@
+import { useParams, Link } from "react-router-dom";
+import "./InventoryEditPage.scss";
+
 function InventoryEditPage() {
-  return <div></div>;
+  return (
+    <section className="edit-inventory">
+      <article className="edit-inventory__heading-container">
+        <Link className="edit-inventory__back-link" to="/inventory"></Link>
+        <h1 className="edit-inventory__heading">Edit Inventory</h1>
+      </article>
+    </section>
+  );
 }
+
 export default InventoryEditPage;
