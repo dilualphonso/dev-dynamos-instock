72f41d3760482563fb784e27aa5c8bb682820836
klkode <katrinalkite@gmail.com>
2024-03-15 23:25:50 -0400

    Create scss file for InventoryAddPage and copy styling from WarehouseAddPage styles. Add heading to InventoryAddPage and link it to a new scss.

--- src/pages/InventoryAddPage/InventoryAddPage.jsx
+++ src/pages/InventoryAddPage/InventoryAddPage.jsx
@@ -1,4 +1,16 @@
+import "./InventoryAddPage.scss";
+import { Link } from "react-router-dom";
+
+
 function WarehousesAddPage() {
-  return <div></div>;
+    return (
+        <section className="add-inventory">
+            <article className="add-inventory__heading-container">
+                <Link className="add-inventory__back-link" to="/inventories"></Link>
+                <h1 className="add-inventory__heading">Add New Inventory Item</h1>
+            </article>
+            
+        </section>
+    );
 }
 export default WarehousesAddPage;
