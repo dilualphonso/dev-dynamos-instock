f43d7e31ef7bcbdb1f39f97de7ef8b5c3575b494
klkode <katrinalkite@gmail.com>
2024-03-15 23:34:10 -0400

    Create InventoryItemForm js and scss files. Add new component to the InventoryAddPage.

---
+++ src/Components/InventoryItemForm/InventoryItemForm.js
@@ -0,0 +1,14 @@
+import "./InventoryItemForm.scss";
+
+
+function InventoryItemForm({itemToEdit}) {
+    
+    return (
+        <section className="inventory-form">
+            <form className="inventory-form__form" name="submitItemForm" id="submitItemForm" > 
+
+            </form>
+        </section>
+    );
+}
+export default InventoryItemForm;
--- src/pages/InventoryAddPage/InventoryAddPage.jsx
+++ src/pages/InventoryAddPage/InventoryAddPage.jsx
@@ -1,15 +1,16 @@
 import "./InventoryAddPage.scss";
 import { Link } from "react-router-dom";
-
+import InventoryItemForm from "../../components/InventoryItemForm/InventoryItemForm";
 
 function WarehousesAddPage() {
+  
     return (
         <section className="add-inventory">
             <article className="add-inventory__heading-container">
                 <Link className="add-inventory__back-link" to="/inventories"></Link>
                 <h1 className="add-inventory__heading">Add New Inventory Item</h1>
             </article>
-            
+            <InventoryItemForm itemToEdit={null} />
         </section>
     );
 }
