e268d2813b90d12001126815701af2b1a3fd63c7
Dilusha Alponso <dilu.alphonso@gmail.com>
2024-03-17 17:37:51 -0400

    updated new branch

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,4 +1,17 @@
+import "./PageNotFound.scss"
+import urlNotFoundImage from "../../assets/images/404-image.webp"
+
 function PageNotFound() {
-  return <div></div>;
+  return (
+
+  <section>
+return (
+        <section className="pageNotFound">
+            <img className="pageNotFound__img" src={urlNotFoundImage} alt="404 Notfound" />
+            <h1 className="pageNotFound__heading">The requested URL was not found on this server.</h1>
+        </section>
+
+    );
+  </section>);
 }
 export default PageNotFound;
