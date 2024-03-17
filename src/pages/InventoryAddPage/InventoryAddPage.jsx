3a63801470c6fa23bf8ce5998c20e2e398115380
klkode <katrinalkite@gmail.com>
2024-03-17 07:17:03 -0400

    Fix bug in back link in InventoryAddPage to navigate to the correct path.

--- src/pages/InventoryAddPage/InventoryAddPage.jsx
+++ src/pages/InventoryAddPage/InventoryAddPage.jsx
@@ -7,7 +7,7 @@ function InventoryAddPage() {
     return (
         <section className="add-inventory">
             <article className="add-inventory__heading-container">
-                <Link className="add-inventory__back-link" to="/inventories"></Link>
+                <Link className="add-inventory__back-link" to="/inventory"></Link>
                 <h1 className="add-inventory__heading">Add New Inventory Item</h1>
             </article>
             <InventoryItemForm itemToEdit={null} />
