524df6ac71dacf1988c26dddfcf897be0db38245
ilaria-longo <157503715+ilaria-longo@users.noreply.github.com>
2024-03-17 22:03:22 -0400

    removing commented out code

--- src/components/InventoryItem/InventoryItem.jsx
+++ src/components/InventoryItem/InventoryItem.jsx
@@ -1,82 +1,97 @@
-import './InventoryItem.scss'
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import { ReactComponent as DeleteIcon } from '../../assets/icons/delete_outline-24px.svg';
-import { ReactComponent as EditIcon } from '../../assets/icons/edit-24px.svg';
-import { ReactComponent as ChevronRightIcon } from '../../assets/icons/chevron_right-24px.svg';
-import InventoryDeletePage from "../../pages/InventoryDeletePage/InventoryDeletePage"
-export const InventoryItem = ({ inventory, warehouseId ,setInventories}) => {
-  const {
-    id,
-    warehouse_name,
-    item_name,
-    category,
-    status,
-    quantity
-  } = inventory;
+import "./InventoryItem.scss";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { ReactComponent as DeleteIcon } from "../../assets/icons/delete_outline-24px.svg";
+import { ReactComponent as EditIcon } from "../../assets/icons/edit-24px.svg";
+import { ReactComponent as ChevronRightIcon } from "../../assets/icons/chevron_right-24px.svg";
+import InventoryDeletePage from "../../pages/InventoryDeletePage/InventoryDeletePage";
+export const InventoryItem = ({ inventory, warehouseId, setInventories }) => {
+  const { id, warehouse_name, item_name, category, status, quantity } =
+    inventory;
 
   const [deleteConfirm, setDeleteConfirm] = useState(false);
   const [selectedInventoryId, setSelectedInventoryId] = useState(null);
   const [selectedInventoryName, setSelectedInventoryName] = useState(null);
 
-  const handleDeleteClick = (inventoryId,inventoryName) => {
+  const handleDeleteClick = (inventoryId, inventoryName) => {
     setDeleteConfirm(true);
     setSelectedInventoryId(inventoryId);
     setSelectedInventoryName(inventoryName);
-    console.log('a popup window is needed');
-  }
+    console.log("a popup window is needed");
+  };
 
   return (
     <section>
-    <li className='inventory'>
-      <div className='inventory__box'>
-        <div className='inventory__container'>
-          <div className='inventory__wrapper'>
-            <div className='inventory__details inventory__details-left'>
-              <div className="inventory__item">
-                <p className="inventory__label">INVENTORY ITEM</p>
-                <Link to={`/inventory/${id}`} className="inventory__value inventory__item-name">
-                  {item_name}
-                  <ChevronRightIcon className="inventory__item-name-icon" />
-                </Link>
+      <li className="inventory">
+        <div className="inventory__box">
+          <div className="inventory__container">
+            <div className="inventory__wrapper">
+              <div className="inventory__details inventory__details-left">
+                <div className="inventory__item">
+                  <p className="inventory__label">INVENTORY ITEM</p>
+                  <Link
+                    to={`/inventory/${id}`}
+                    className="inventory__value inventory__item-name"
+                  >
+                    {item_name}
+                    <ChevronRightIcon className="inventory__item-name-icon" />
+                  </Link>
+                </div>
+                <div className="inventory__category">
+                  <p className="inventory__label">CATEGORY</p>
+                  <p className="inventory__value">{category}</p>
+                </div>
               </div>
-              <div className="inventory__category">
-                <p className="inventory__label">CATEGORY</p>
-                <p className="inventory__value">{category}</p>
+              <div className="inventory__details inventory__details-right">
+                <div className="inventory__status">
+                  <p className="inventory__label">STATUS</p>
+                  <p
+                    className={`inventory__value inventory__status-text ${
+                      quantity === 0 ? "red-text" : "green-text"
+                    }`}
+                  >
+                    {status}
+                  </p>
+                </div>
+                <div className="inventory__quantity">
+                  <p className="inventory__label">QTY</p>
+                  <p className="inventory__value">{quantity}</p>
+                </div>
               </div>
             </div>
-            <div className="inventory__details inventory__details-right">
-              <div className="inventory__status">
-                <p className="inventory__label">STATUS</p>
-                <p className={`inventory__value inventory__status-text ${quantity === 0 ? 'red-text' : 'green-text'}`}>{status}</p>
-              </div>
-              <div className="inventory__quantity">
-                {/* /inventories/: desktop/tablet/mobile:QTY,  */}
-                {/* /warehouses/1/inventories: desktop:QUANTITY; tablet/mobile:QTY,  */}
-                <p className="inventory__label">QTY</p>
-                {/* <p>QUANTITY</p> */}
-                <p className="inventory__value">{quantity}</p>
-              </div>
+            <div
+              className={`inventory__warehouse ${
+                warehouseId ? "inventory__warehouse--has-warehouse" : ""
+              }`}
+            >
+              <p className="inventory__label">WAREHOUSE</p>
+              <p className="inventory__value">{warehouse_name}</p>
             </div>
           </div>
-          <div className={`inventory__warehouse ${warehouseId ? 'inventory__warehouse--has-warehouse' : ''}`}>
-            <p className="inventory__label">WAREHOUSE</p>
-            <p className="inventory__value">{warehouse_name}</p>
+          <div className="inventory__actions">
+            <figure
+              className="inventory__action-button inventory__action-delete-button"
+              onClick={() => handleDeleteClick(id, item_name)}
+            >
+              <DeleteIcon />
+            </figure>
+            <Link
+              to={`/inventory/${id}/edit`}
+              className="inventory__action-button inventory__action-edit-button"
+            >
+              <EditIcon />
+            </Link>
           </div>
         </div>
-        <div className="inventory__actions">
-          <figure className="inventory__action-button inventory__action-delete-button" onClick={() => handleDeleteClick(id,item_name)}>
-            <DeleteIcon />
-          </figure>
-          <Link to={`/inventory/${id}/edit`} className="inventory__action-button inventory__action-edit-button">
-            <EditIcon />
-          </Link>
-        </div>
-      </div>
-
-    </li>
-    {deleteConfirm && <InventoryDeletePage setDeleteConfirm={setDeleteConfirm} inventoryId={selectedInventoryId} inventoryName={selectedInventoryName} setInventories={setInventories}/>}
+      </li>
+      {deleteConfirm && (
+        <InventoryDeletePage
+          setDeleteConfirm={setDeleteConfirm}
+          inventoryId={selectedInventoryId}
+          inventoryName={selectedInventoryName}
+          setInventories={setInventories}
+        />
+      )}
     </section>
-
-  )
-}
+  );
+};
--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,118 +1,154 @@
-import './WarehouseList.scss';
+import "./WarehouseList.scss";
 
-import arrowImage from "../../assets/icons/chevron_right-24px.svg"
-import deleteIcon from "../../assets/icons/delete_outline-24px.svg"
-import editIcon from "../../assets/icons/edit-24px.svg"
-import WarehouseDeletePage from "../../pages/WarehouseDeletePage/WarehouseDeletePage"
-import { useState } from 'react';
-import { Link} from 'react-router-dom';
+import arrowImage from "../../assets/icons/chevron_right-24px.svg";
+import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
+import editIcon from "../../assets/icons/edit-24px.svg";
+import WarehouseDeletePage from "../../pages/WarehouseDeletePage/WarehouseDeletePage";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import Sorting from "../Sorting/Sorting";
 
-
-function WarehouseList({ warehouses , setWarehouses, handleAscClick, handleDescClick}) {
-
+function WarehouseList({
+  warehouses,
+  setWarehouses,
+  handleAscClick,
+  handleDescClick,
+}) {
   const [selectedWarehouseId, setSelectedWarehouseId] = useState(null);
   const [selectedWarehouseName, setSelectedWarehouseName] = useState(null);
   const [modalOpen, setModalOpen] = useState(false);
 
-  const handleDeleteClick = (warehouseId,warehouseName) => {
+  const handleDeleteClick = (warehouseId, warehouseName) => {
     setSelectedWarehouseId(warehouseId);
     setSelectedWarehouseName(warehouseName);
     setModalOpen(true);
   };
 
-
-
-
   return (
     <section className="warehouse">
-
-      <div className= "warehouse__box">
+      <div className="warehouse__box">
         <div className="warehouse__tab-headings">
           <div className="warehouse__headings-name">
             <h4 className="warehouse__name-heading">Warehouse</h4>
-            <Sorting onAscClick={() => handleAscClick('warehouse_name')} onDescClick={() => handleDescClick('warehouse_name')} />
+            <Sorting
+              onAscClick={() => handleAscClick("warehouse_name")}
+              onDescClick={() => handleDescClick("warehouse_name")}
+            />
           </div>
           <div className="warehouse__headings-address">
             <h4 className="warehouse__address-heading">Address</h4>
-            <Sorting onAscClick={() => handleAscClick('address,city,country')} onDescClick={() => handleDescClick('address,city,country')} />
+            <Sorting
+              onAscClick={() => handleAscClick("address,city,country")}
+              onDescClick={() => handleDescClick("address,city,country")}
+            />
           </div>
           <div className="warehouse__headings-contact-name">
             <h4 className="warehouse__contact-name-heading">Contact Name</h4>
-            <Sorting onAscClick={() => handleAscClick('contact_name')} onDescClick={() => handleDescClick('contact_name')} />
+            <Sorting
+              onAscClick={() => handleAscClick("contact_name")}
+              onDescClick={() => handleDescClick("contact_name")}
+            />
           </div>
           <div className="warehouse__headings-contact-info">
-            <h4 className="warehouse__contact-info-heading">Contact Information</h4>
-            <Sorting onAscClick={() => handleAscClick('contact_phone,contact_email')} onDescClick={() => handleDescClick('contact_phone,contact_email')} />
+            <h4 className="warehouse__contact-info-heading">
+              Contact Information
+            </h4>
+            <Sorting
+              onAscClick={() => handleAscClick("contact_phone,contact_email")}
+              onDescClick={() => handleDescClick("contact_phone,contact_email")}
+            />
           </div>
-
           <h4 className="warehouse__actions">Actions</h4>
-
-          {/* <img className="warehouse__sort" src={sortIcon} alt="sorting icon" /> */}
         </div>
 
-
-        {warehouses.length >0 && warehouses
-.map((warehouse) => (
-
-          <div className="warehouse__test" key={warehouse.id}>
-
-            <div className='warehouse__wrapper'>
-
-              <div className="warehouse__container">
-
-                <div className="warehouse__left-container">
-                  <div className="warehouse__name-container">
-
-                    <h4 className="warehouse__name-label">Warehouse</h4>
-               <Link to ={`/warehouses/${warehouse.id}`}> <div className="warehouse__arrow-wrapper">
-                      <div className="warehouse__name">{warehouse.warehouse_name}</div>
-                      <img className="warehouse__arrowImg" src={arrowImage} alt="arrowImage" />
-                    </div></Link>
-
-
-                  </div>
-                  <div className="warehouse__address-container">
-                    <h4 className="warehouse__address-label">Address</h4>
-                    <p className="warehouse__address">{warehouse.address}, {warehouse.city}, {warehouse.country}</p>
-                  </div>
-
-                </div>
-                <div className="warehouse__right-container">
-                  <div className="warehouse__contact-name-container">
-                    <h4 className="warehouse__contact-name-label">Contact Name</h4>
-                    <div className="warehouse__contact-name">{warehouse.contact_name}</div>
-                  </div>
-
-                  <div className="warehouse__contact-info-container">
-                    <h4 className="warehouse__contact-info-label">Contact Information</h4>
-                    <div className='warehouse__contact-info'>
-                      <div className="warehouse__contact-phone" >{warehouse.contact_phone}</div>
-                      <div className="warehouse__contact-email">{warehouse.contact_email}</div>
+        {warehouses.length > 0 &&
+          warehouses.map((warehouse) => (
+            <div className="warehouse__test" key={warehouse.id}>
+              <div className="warehouse__wrapper">
+                <div className="warehouse__container">
+                  <div className="warehouse__left-container">
+                    <div className="warehouse__name-container">
+                      <h4 className="warehouse__name-label">Warehouse</h4>
+                      <Link to={`/warehouses/${warehouse.id}`}>
+                        {" "}
+                        <div className="warehouse__arrow-wrapper">
+                          <div className="warehouse__name">
+                            {warehouse.warehouse_name}
+                          </div>
+                          <img
+                            className="warehouse__arrowImg"
+                            src={arrowImage}
+                            alt="arrowImage"
+                          />
+                        </div>
+                      </Link>
+                    </div>
+                    <div className="warehouse__address-container">
+                      <h4 className="warehouse__address-label">Address</h4>
+                      <p className="warehouse__address">
+                        {warehouse.address}, {warehouse.city},{" "}
+                        {warehouse.country}
+                      </p>
                     </div>
                   </div>
+                  <div className="warehouse__right-container">
+                    <div className="warehouse__contact-name-container">
+                      <h4 className="warehouse__contact-name-label">
+                        Contact Name
+                      </h4>
+                      <div className="warehouse__contact-name">
+                        {warehouse.contact_name}
+                      </div>
+                    </div>
 
+                    <div className="warehouse__contact-info-container">
+                      <h4 className="warehouse__contact-info-label">
+                        Contact Information
+                      </h4>
+                      <div className="warehouse__contact-info">
+                        <div className="warehouse__contact-phone">
+                          {warehouse.contact_phone}
+                        </div>
+                        <div className="warehouse__contact-email">
+                          {warehouse.contact_email}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
+                <div className="warehouse__icons">
+                  <button
+                    className="warehouse__delete-btn"
+                    onClick={() =>
+                      handleDeleteClick(warehouse.id, warehouse.warehouse_name)
+                    }
+                  >
+                    <img
+                      className="warehouse__delete-icon"
+                      src={deleteIcon}
+                      alt="delete-trash"
+                    />
+                  </button>
 
-
+                  <Link to={`/warehouses/${warehouse.id}/edit`}>
+                    {" "}
+                    <button className="warehouse__edit-icon">
+                      <img src={editIcon} alt="edit-icon" />
+                    </button>{" "}
+                  </Link>
+                </div>
               </div>
-              <div className='warehouse__icons'>
-          <button className= "warehouse__delete-btn" onClick={() => handleDeleteClick(warehouse.id,warehouse.warehouse_name)}><img className="warehouse__delete-icon" src={deleteIcon} alt="delete-trash" /></button>
-
-             <Link to={`/warehouses/${warehouse.id}/edit`} >  <button className="warehouse__edit-icon"><img src={editIcon} alt="edit-icon" /></button> </Link>
-              </div>
-
             </div>
-
-          </div>
-
-        ))
-      }
-
-
+          ))}
       </div>
-      {modalOpen && <WarehouseDeletePage setOpenModal={setModalOpen} warehouseId={selectedWarehouseId} warehouseName={selectedWarehouseName} setWarehouses={setWarehouses}/>}
-
+      {modalOpen && (
+        <WarehouseDeletePage
+          setOpenModal={setModalOpen}
+          warehouseId={selectedWarehouseId}
+          warehouseName={selectedWarehouseName}
+          setWarehouses={setWarehouses}
+        />
+      )}
     </section>
   );
 }
